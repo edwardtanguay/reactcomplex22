b2cfda1412f9c88b3791838447de4f7cc2978935
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-10-25 23:01:06 +0100

    add animate.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import Card from './components/Card'
+
+// import Animate from 'animate.css-react'
+// import 'animate.css/animate.css' 
+
+const duration = "test";
 
 class App extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			toggleLogo: true
+			toggleLogo: true,
+			cards: [
+				{
+					id: 1
+				},
+				{
+					id: 2
+				},
+				{
+					id: 3
+				},
+				{
+					id: 4
+				},
+				{
+					id: 5
+				},
+				{
+					id: 6
+				}
+			]
 		};
 
 		// need for ES6
@@ -23,13 +49,33 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				{/* <Animate
+					appear="fadeInDown"
+					durationAppear={1000}
+					component="div" >
+
+					<h1>react animate.css</h1>
+
+				</Animate> */}
+
+				<h1 class="animate__animated animate__bounce">An animated element</h1>
+
+
 				<header className="App-header">
 					<img src={logo}
-						className={this.state.toggleLogo ? 'static-logo' : 'animated-logo'}
-						onClick={this.toggleLogo}
+						className={this.state.toggleLogo ? 'static-logo' : 'static-logo animate__animated animate__jello'}
+						onMouseEnter={this.toggleLogo}
+						onMouseLeave={this.toggleLogo}
 						alt="logo" />
 					<div><span className="mainHeader">Welcome to React</span></div>
 				</header>
+				<div className="grid">
+					{this.state.cards.map((card) => {
+						return (
+							<Card duration={duration} key={card.id}/>
+						)
+					})}
+				</div>
 			</div>
 		);
 	}
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => (
+	<div className="card">
+		<img src="juice.jpg" alt="Juice" className="card-image" />
+		<div className="container">
+			<h3>Vitamin Juice <span className="price">$3.99</span></h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dignissimos cumque soluta id repellat error non perspiciatis vero ad facilis mollitia delectus, iste consequuntur quaerat, minus ab eligendi? Vero rem quos maxime et, eius repellat earum.</p>
+		</div>
+	</div>
+);
+
+export default Card;
