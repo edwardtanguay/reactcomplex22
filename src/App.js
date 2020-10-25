5373c58131c486839951166f686b08e583ebdda4
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-10-25 04:04:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			toggleLogo: true
+		};
+
+		// need for ES6
+		this.toggleLogo = this.toggleLogo.bind(this);
+	}
+
+	toggleLogo(event) {
+		this.setState((prevState) => ({
+			toggleLogo: !prevState.toggleLogo
+		}));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo}
+						className={this.state.toggleLogo ? 'static-logo' : 'animated-logo'}
+						onClick={this.toggleLogo}
+						alt="logo" />
+					<div><span className="mainHeader">Welcome to React</span></div>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
